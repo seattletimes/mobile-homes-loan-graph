22166d98396ef653e6601bacbf65adfa02a0b3b8
Thomas Wilburn <news@thomaswilburn.net>
2015-03-31 14:16:27 -0700

    Chatter updates from Mike.

--- src/js/presets.js
+++ src/js/presets.js
@@ -1,6 +1,6 @@
 module.exports = {
   "Ackley": {
-    description: "Short description of the Ackley case",
+    description: "Kirk and Patricia Ackley, of Ephrata, Wash., bought a Clayton home in 2006 despite a last-minute change in promised loan terms.",
     term: 24,
     interest: 12.86,
     price: 96005.56,
@@ -9,7 +9,7 @@ module.exports = {
     depreciates: true
   },
   "Mansfield": {
-    description: "Short description of the Mansfield case",
+    description: "After losing her previous home to a tornado, Dorothy Mansfield, of Bladenboro, N.C., bought a home from Clayton in 2011.",
     term: 20,
     interest: 10.13,
     price: 56734,
@@ -18,7 +18,7 @@ module.exports = {
     depreciates: true
   },
   "Galler": {
-    description: "Short description of the Galler case",
+    description: "Tiffany Galler was a single mother living in Crestview, Fla., in 2005, when she purchased a mobile home with a Clayton loan.",
     term: 20,
     interest: 11.67,
     price: 37195,
@@ -27,7 +27,7 @@ module.exports = {
     depreciates: true
   },
   "trailer": {
-    description: "Simulates a typical mobile home purchase.",
+    description: "Explore how interest rates, loan terms, down payments and costs shape a borrower’s loan experience.",
     term: 24,
     interest: 10,
     price: 80000,
@@ -35,14 +35,5 @@ module.exports = {
     down: 10,
     customize: true,
     depreciates: true
-  },
-  "house": {
-    description: "This preset simulates home ownership in a market where values are stable, meaning that the home neither appreciates nor depreciates over time.",
-    term: 30,
-    interest: 3.78,
-    price: 300000,
-    other: 2000,
-    down: 20,
-    customize: true
   }
 };
