a10b7ce66d24dcec5e4fff80b1c5ee8c723f0bd2
Thomas Wilburn <news@thomaswilburn.net>
2015-04-01 12:18:36 -0700

    Last tweaks from Laura/Whitney

--- src/js/presets.js
+++ src/js/presets.js
@@ -1,6 +1,6 @@
 module.exports = {
   "Ackley": {
-    description: "Kirk and Patricia Ackley, of Ephrata, Wash., bought a Clayton home in 2006 despite a last-minute change in promised loan terms.",
+    description: "Kirk and Patricia Ackley, of Ephrata, Wash., bought a Clayton home in 2006 despite a last-minute change in promised loan terms. They asked to refinance at the promised rate, but their Clayton lender repossessed their home.",
     term: 24,
     interest: 12.5,
     price: 96005.56,
@@ -9,7 +9,7 @@ module.exports = {
     depreciates: true
   },
   "Mansfield": {
-    description: "After losing her previous home to a tornado, Dorothy Mansfield, of Bladenboro, N.C., bought a home from Clayton in 2011.",
+    description: "After losing her previous home to a tornado, Dorothy Mansfield, of Bladenboro, N.C., bought a home from Clayton in 2011. Within 18 months, her lender was trying to foreclose on her home and land.",
     term: 20,
     interest: 9.74,
     price: 56734,
@@ -18,7 +18,7 @@ module.exports = {
     depreciates: true
   },
   "Galler": {
-    description: "Tiffany Galler was a single mother living in Crestview, Fla., in 2005, when she purchased a mobile home with a Clayton loan.",
+    description: "Tiffany Galler was a single mother living in Crestview, Fla., in 2005, when she purchased a mobile home with a Clayton loan. After paying more than the sales price over eight years of payments, her home was repossessed.",
     term: 20,
     interest: 10.5,
     price: 37195,
@@ -27,7 +27,7 @@ module.exports = {
     depreciates: true
   },
   "trailer": {
-    description: "Explore how interest rates, loan terms, down payments and costs shape a borrower’s loan experience, by setting your own values below.",
+    description: "Set your own values below to explore how interest rates, loan terms, down payments and costs shape a borrower’s loan experience.",
     term: 24,
     interest: 10,
     price: 80000,
